64f3bee4693cbcfb2f9c16f3037dde4be0419272
Ian Riley <ian@riley.asia>
2014-08-30 00:42:33 +0200

    file from MoinMoin wiki

--- RenderAsRestructuredtext.py
+++ RenderAsRestructuredtext.py
@@ -1,16 +1,14 @@
 """
-    MoinMoin - Render as reStructuredText action - redirects to the
-    reStructuredText formatter
+    MoinMoin - Render as reStructuredText action - redirects to the reStructuredText formatter
 
     @copyright: 2008 Stefan Merten
     @license: GNU GPL, see COPYING for details.
 """
+# Modified for MoinMoin Version 1.9 by IanRiley 2012-01-04
 
 from MoinMoin.Page import Page
-from MoinMoin.util import MoinMoinNoFooter
 
 def execute(pagename, request):
-    url = Page(request, pagename).url(request, {'action': 'format',
-                                                'mimetype': 'text/x-rst'}, 0)
-    request.http_redirect(url)
-    raise MoinMoinNoFooter
+    url = Page(request, pagename).url(request, {'action': 'format', 'mimetype': 'text/x-rst'})
+    return request.http_redirect(url)
+
